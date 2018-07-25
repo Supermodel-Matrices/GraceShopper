29986ba82c4873ef8473e78b5611caaef75db38f
Jessica <23249301+hsujessica@users.noreply.github.com>
2018-07-25 17:52:14 -0400

    user api routes

--- server/api/user.js
+++ server/api/user.js
@@ -38,8 +38,8 @@ router.put('/:id', async (req, res, next) => {
 
 router.post('/:id', async (req, res, next) => {
   try {
-    const newUser = await User.create(req.body);
-    res.status(201).send(newUser);
+    const user = await User.create(req.body);
+    res.status(201).send(user);
   }
   catch (err) {
     next(err);
