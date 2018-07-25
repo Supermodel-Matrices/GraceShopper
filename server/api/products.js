c582a1994def8ec71b47f0af3cc1e2a4de37b0ba
Jessica Hsu <23249301+hsujessica@users.noreply.github.com>
2018-07-25 17:05:41 -0400

    Update products.js

--- server/api/products.js
+++ server/api/products.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { Product } = require('../db');
 
-router.get('/api/products', async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
     const allProducts = await Product.findAll();
     res.send(allProducts);
