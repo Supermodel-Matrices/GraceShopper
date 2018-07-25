71d5b6e6674f2ea4cab0b35e439fff5d9cefc60f
Jessica <23249301+hsujessica@users.noreply.github.com>
2018-07-25 17:56:40 -0400

    index api

---
+++ server/api/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+// router.use('/cart', require('./cart'));
+// router.use('/products', require('./products'));
+router.use('/user', require('./user'));
+
+router.use((req, res, next) => {
+  const err = new Error('API route not found');
+  err.status = 404;
+  next(err);
+});
+
+module.exports = router;
