9ff0d11d6bad08a894576aeb833f14e036579edb
Cheryl Ng <cherylngql@gmail.com>
2018-07-25 14:44:53 -0400

    modify scripts

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,18 @@
-const Sequelize = require('sequelize');
+const Sequelize = require('sequelize')
+const pkg = require('../../package.json')
 
-const db = new Sequelize('postgres://localhost:5432/graceshopper', {
-  logging: false // unless you like the logs
-  // ...and there are many other options you may want to play with
-});
+const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
-module.exports = db;
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  {
+    logging: false
+  }
+)
+module.exports = db
+
+// This is a global Mocha hook used for resource cleanup.
+// Otherwise, Mocha v4+ does not exit after tests.
+if (process.env.NODE_ENV === 'test') {
+  after('close database connection', () => db.close())
+}
