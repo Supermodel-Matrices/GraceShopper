2b14542dc611ad2e78e17b9a2e00b330b952542a
Jessica Hsu <23249301+hsujessica@users.noreply.github.com>
2018-07-25 17:04:53 -0400

    no puppies :(

--- server/api/products.js
+++ server/api/products.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const { Product } = require('../db');
 
-// matches GET requests to /api/puppies/
 router.get('/api/products', async (req, res, next) => {
   try {
     const allProducts = await Product.findAll();
