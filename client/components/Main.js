436bd98c6029a51f7b0f5de7d8f79365d79b3e43
Cheryl Ng <cherylngql@gmail.com>
2018-07-26 14:37:10 -0400

    some css

--- client/components/Main.js
+++ client/components/Main.js
@@ -11,15 +11,19 @@ const Main = () => {
   return (
     <Router>
 			<div id="main">
-			<NavBar />
-				<Switch>
-					<Route exact path="/" component={AllProducts} />
-					<Route exact path="/products" component={AllProducts} />
-					<Route exact path="/products/:id" component={SingleProduct} />
-					<Route exact path="/cart" component={CartPage} />
-					<Route exact path="/login" component={Login} />
-					<Route exact path="/signup" component={Signup} />
-				</Switch>
+			  <div id="navbar">
+					<NavBar />
+				</div>
+				<div id="component">
+					<Switch>
+						<Route exact path="/" component={AllProducts} />
+						<Route exact path="/products" component={AllProducts} />
+						<Route exact path="/products/:id" component={SingleProduct} />
+						<Route exact path="/cart" component={CartPage} />
+						<Route exact path="/login" component={Login} />
+						<Route exact path="/signup" component={Signup} />
+					</Switch>
+				</div>
 			</div>
     </Router>
   )
