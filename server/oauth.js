5e5b94d17c90e60e8bca3486963d9785441c4e3f
Cheryl Ng <cherylngql@gmail.com>
2018-07-28 10:41:01 -0400

    oauth working user model modified

--- server/oauth.js
+++ server/oauth.js
@@ -30,7 +30,7 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
 
   passport.use(strategy);
 
-  router.get('/google', passport.authenticate('google', {scope: 'email'}))
+  router.get('/', passport.authenticate('google', {scope: 'email'}))
 
   router.get(
     '/callback',
