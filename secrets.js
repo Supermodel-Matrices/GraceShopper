abaa78ac9fa1a9bdf9fa990b4163a52f3b5c79a3
Jessica <23249301+hsujessica@users.noreply.github.com>
2018-07-27 20:41:26 -0400

    switch branches

---
+++ secrets.js
@@ -0,0 +1,3 @@
+process.env.GOOGLE_CLIENT_ID = '303069917453-2th94699c17bfmbdemr7br98m8sqb1go.apps.googleusercontent.com'
+process.env.GOOGLE_CLIENT_SECRET = 'tr49Zd2L2rztN_b4SX7fViIg'
+process.env.GOOGLE_CALLBACK = '/auth/google/callback'
