98d3978bb0192c7e3303bffdb8cd72d59b4d9556
Jessica Hsu <23249301+hsujessica@users.noreply.github.com>
2018-07-28 18:11:37 -0400

    why is this
    file still being pushed despite being unstaged and on gitignore ???

--- secrets.js
+++
@@ -1,3 +0,0 @@
-process.env.GOOGLE_CLIENT_ID = '303069917453-2th94699c17bfmbdemr7br98m8sqb1go.apps.googleusercontent.com'
-process.env.GOOGLE_CLIENT_SECRET = 'tr49Zd2L2rztN_b4SX7fViIg'
-process.env.GOOGLE_CALLBACK = '/auth/google/callback'
