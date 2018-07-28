d6b1b301f5c1e2209faa21e452c87f3801d68be0
Cheryl Ng <cherylngql@gmail.com>
2018-07-28 11:05:16 -0400

    secrets

---
+++ secrets.js
@@ -0,0 +1,3 @@
+process.env.GOOGLE_CLIENT_ID = '989806601889-031f0rinsm2e5vrpi8hbelai690114r5.apps.googleusercontent.com';
+process.env.GOOGLE_CLIENT_SECRET = 'cnKeJ93c6uKJD7x38Cv0Ql6P';
+process.env.GOOGLE_CALLBACK = '/auth/google/callback';
