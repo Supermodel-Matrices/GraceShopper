83f59b8fd8b0229cf6e4b69c8d91e33268d3f554
Cheryl Ng <cherylngql@gmail.com>
2018-07-28 11:02:22 -0400

    force true to clear heroku database

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const {db} = require('./server/db');
 const app = require('./server');
 const port = process.env.PORT || 3000;
 
-db.sync().then(function(){
+db.sync({force: true}).then(function(){
   app.listen(port, function () {
     console.log(`Your server, listening on port ${port}`);
   }) // then start listening with our express server once we have synced
