bee7990292034e9a2333cf6d618f1667dcf05603
AsherLMac <renpsybeta@gmail.com>
2018-07-29 09:22:50 -0400

    New Route To Checkout Page Added"

--- secrets.js
+++ secrets.js
@@ -1,3 +1,3 @@
-process.env.GOOGLE_CLIENT_ID = '989806601889-031f0rinsm2e5vrpi8hbelai690114r5.apps.googleusercontent.com';
-process.env.GOOGLE_CLIENT_SECRET = 'cnKeJ93c6uKJD7x38Cv0Ql6P';
-process.env.GOOGLE_CALLBACK = '/auth/google/callback';
+process.env.GOOGLE_CLIENT_ID = '612020846612-v79iphnael20glke7r4b2c4931mqv3na.apps.googleusercontent.com'
+process.env.GOOGLE_CLIENT_SECRET = '0eA20W0SOcHSRuc1JCR8v9zk'
+process.env.GOOGLE_CALLBACK = '/auth/google/callback'
