9a311790b8bd7fba5db7b0c5fcf4c27462e10002
Jessica <23249301+hsujessica@users.noreply.github.com>
2018-07-29 18:32:29 -0400

    nav change to class component for component did mount

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,36 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Link, withRouter} from 'react-router-dom';
 import {logoutUser} from '../store/user';
+import {getCart} from '../store/cart';
 
-const NavBar = (props) => {
-	let cartQuantities = (Object.values(props.cart));
-	let cartCount = cartQuantities.length ? cartQuantities.reduce((acc, i) => acc + i, 0) : 0;
-	return (
-		<div className="nav-container">
-			<div className="nav-top">
-			<nav>
-			  <Link className="home-link undecorated-link" to="/">CORMORANT</Link>
-				<Link to="/products">All</Link>
-				<Link to="/products/category/lighting">Lighting</Link>
-				<Link to="/products/category/greenery">Greenery</Link>
-				<Link to="/products/category/textiles">Textiles</Link>
-				<Link to="/products/category/walldecor">Wall Decor</Link>
-				<Link to="/cart"><img src="./cart.png" className="cart-icon" /><span className="cart-count">{cartCount}</span>
-				</Link>
-			</nav>
-			</div>
-			<div className="nav-bottom">
-				{props.user.id ?
-				<div>
-					<Link to={`/user/${props.user.id}`}>Hi {props.user.name}!</Link>
-					<button type="button" onClick={props.logout} className="btn-main">Logout</button>
-				</div> :
-				<span><Link to="/login">Login</Link> / <Link to="/signup">Signup</Link></span>}
-				<Link to="/contact">Contact</Link>
-			</div>
-		</div>
-	);
+export class NavBar extends Component {
+
+	componentDidMount () {
+		this.props.getCart();
+	}
+
+	render () {
+		let cartQuantities = (Object.values(this.props.cart));
+		let cartCount = cartQuantities.length ? cartQuantities.reduce((acc, i) => acc + i, 0) : 0;
+			return (
+				<div className="nav-container">
+					<div className="nav-top">
+					<nav>
+						<Link className="home-link undecorated-link" to="/">CORMORANT</Link>
+						<Link to="/products">All</Link>
+						<Link to="/products/category/lighting">Lighting</Link>
+						<Link to="/products/category/greenery">Greenery</Link>
+						<Link to="/products/category/textiles">Textiles</Link>
+						<Link to="/products/category/walldecor">Wall Decor</Link>
+						<Link to="/cart"><img src="./cart.png" className="cart-icon" /><span className="cart-count">{cartCount}</span>
+						</Link>
+					</nav>
+					</div>
+					<div className="nav-bottom">
+						{this.props.user.id ?
+						<div>
+							<Link to={`/user/${this.props.user.id}`}>Hi {this.props.user.name}!</Link>
+							<button type="button" onClick={this.props.logout} className="btn-main">Logout</button>
+						</div> :
+						<span><Link to="/login">Login</Link> / <Link to="/signup">Signup</Link></span>}
+						<Link to="/contact">Contact</Link>
+					</div>
+				</div>
+			)
+		}
 }
 
 const mapStateToProps = (state) => ({
@@ -41,7 +49,8 @@ const mapDispatchToProps = (dispatch, ownProps) => ({
   logout: () => {
 		dispatch(logoutUser());
 		ownProps.history.push('/');
-	}
+	},
+	getCart: () => dispatch(getCart())
 });
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(NavBar));
