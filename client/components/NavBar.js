773443b112f4e85d806b1ae89bf483aa8e1b0926
Jessica <23249301+hsujessica@users.noreply.github.com>
2018-07-30 11:19:16 -0400

    nav bar css

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -17,23 +17,22 @@ export class NavBar extends Component {
 					<div className="nav-top">
 					<nav>
 						<Link className="home-link undecorated-link" to="/">CORMORANT</Link>
-						<Link to="/products">All</Link>
-						<Link to="/products/category/lighting">Lighting</Link>
-						<Link to="/products/category/greenery">Greenery</Link>
-						<Link to="/products/category/textiles">Textiles</Link>
-						<Link to="/products/category/wall-decor">Wall Decor</Link>
-						<Link to="/cart"><img src="https://cdn1.iconfinder.com/data/icons/social-productivity-line-art-4/128/shopping-cart2-512.pn" className="cart-icon" /><span className="cart-count">{cartCount}</span>
-						</Link>
+							<p className="nav-link"><Link to="/products">All</Link></p>
+							<p className="nav-link"><Link to="/products/category/lighting">Lighting</Link></p>
+							<p className="nav-link"><Link to="/products/category/greenery">Greenery</Link></p>
+							<p className="nav-link"><Link to="/products/category/textiles">Textiles</Link></p>
+							<p className="nav-link"><Link to="/products/category/wall-decor">Wall Decor</Link></p>
+							<p className="nav-link"><Link to="/cart"><img src="https://cdn1.iconfinder.com/data/icons/social-productivity-line-art-4/128/shopping-cart2-512.png" className="cart-icon" />&nbsp;&nbsp;&nbsp;&nbsp;<span className="cart-count">{cartCount}</span></Link></p>
 					</nav>
 					</div>
 					<div className="nav-bottom">
-						{this.props.user.id ?
-						<div>
-							<Link to={`/user/${this.props.user.id}`}>Hi {this.props.user.name}!</Link>
-							<button type="button" onClick={this.props.logout} className="btn-main">Logout</button>
-						</div> :
-						<span><Link to="/login">Login</Link> / <Link to="/signup">Signup</Link></span>}
-						<Link to="/contact">Contact</Link>
+					{this.props.user.id ?
+				<div>
+					<p className="nav-link"><Link to={`/user/${this.props.user.id}`}>Hi {this.props.user.name}!</Link></p>
+					<button type="button" onClick={this.props.logout} className="btn-main">Logout</button>
+				</div> :
+				<p className="nav-link"><span><Link to="/login">Login</Link>&nbsp;&nbsp;/&nbsp;&nbsp;<Link to="/signup">Signup</Link></span></p>}
+				<p className="nav-link"><Link to="/contact">Contact</Link></p>
 					</div>
 				</div>
 			)
