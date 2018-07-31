b45d053ad3eaaf833a05b6d494c6771c396d79be
Cheryl Ng <cherylngql@gmail.com>
2018-07-30 22:03:10 -0400

    merge

--- client/store/user.js
+++ client/store/user.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const LOGIN_USER = 'LOGIN_USER';
 const LOG_OUT = 'LOG_OUT';
+const UPDATE_USER = 'UPDATE_USER';
 
 const initialState = {};
 
@@ -10,7 +11,9 @@ export const userReducer = (state = initialState, action) => {
 		case LOGIN_USER:
 			return action.user;
 		case LOG_OUT:
-		  return {};
+			return {};
+		case UPDATE_USER:
+		return action.user;
 		default:
 		  return state;
 	}
@@ -23,7 +26,12 @@ const loginUser = (user) => ({
 
 const logout = () => ({
 	type: LOG_OUT
-})
+});
+
+const updatedUser = (user) => ({
+	type: UPDATE_USER,
+	user
+});
 
 export const login = (formData) => async dispatch => {
 	let response;
@@ -37,6 +45,7 @@ export const login = (formData) => async dispatch => {
 };
 
 export const signup = (formData) => async dispatch => {
+<<<<<<< HEAD
 	let response;
 	try {
 		response = await axios.post('/auth/signup', formData);
@@ -46,15 +55,27 @@ export const signup = (formData) => async dispatch => {
 		return err.response.status;
 	}
 }
+=======
+	const response = await axios.post('/auth/signup', formData);
+	dispatch(loginUser(response.data));
+};
+>>>>>>> user can edit name
 
 export const getLoggedInUser = () => async dispatch => {
 	const response = await axios.get('/auth/me');
 	const loggedInUser = response.data;
 	dispatch(loginUser(loggedInUser));
 	return loggedInUser;
-}
+};
 
 export const logoutUser = () => async dispatch => {
 	await axios.delete('/auth/logout');
 	dispatch(logout());
+};
+
+export const updateUser = (update) => async dispatch => {
+	const response = await axios.put(`/api/user/${update.id}`, update);
+	const updated = response.data;
+	dispatch(updatedUser(updated));
+	return updated;
 };
