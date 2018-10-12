fb0967e891accc312a749bce4bb075d6233de5be
Cheryl Ng <cherylngql@gmail.com>
2018-10-12 18:22:24 -0400

    re-added home image

--- client/components/Home.js
+++ client/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => (
   <div>
-    <img src = "https://www.studiofmp.com/wp-content/uploads/2018/6/interiors-georgia-o-keeffe-lisa-says-gah.jpg" className="home-bg" />
+    <img src = "okeefe.jpg" className="home-bg" />
   </div>
 );
 
